e17f7f02942ba52aa124d5fe53ffcee36a5d7c05
Babar <babarsmoke24@gmail.com>
2025-09-28 19:04:06 +0530

    feat  : Header Component implelmented

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ const Header = () => {
   return (
     <div>
       <h1 className="text-2xl text-center font-bold font-mono md:text-3xl">
-        What should I help you Change???
+        What should I help you Refine???
       </h1>
     </div>
   );
