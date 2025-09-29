5b0c6189a669444119c5ec226972210d08f34103
Babar <babarsmoke24@gmail.com>
2025-09-29 16:39:19 +0530

    Refactor: Lifting the state up implemented for tone

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -7,7 +7,7 @@ import { useState } from "react";
 import { Button } from "@/components/ui/button";
 
 import { Textarea } from "@/components/ui/textarea";
-import { ChevronRight } from "lucide-react";
+import { CheckCheck, ChevronRight, Copy, Loader2 } from "lucide-react";
 import { useForm } from "react-hook-form";
 import { ScrollArea } from "./ui/scroll-area";
 import CustomDropDown from "./CustomDropDown";
@@ -19,27 +19,38 @@ const FormSchema = z.object({
 });
 
 const MainContent = () => {
-  // const { data } = useSession();
+  const [copy, setCopied] = useState<"Copy" | "Copied">("Copy");
+  const [tone, setTone] = useState<string>("");
+  const [loading, setLoading] = useState(false);
+
   const form = useForm<z.infer<typeof FormSchema>>({
     resolver: zodResolver(FormSchema),
   });
-
-  const HandleSubmit = async (data: z.infer<typeof FormSchema>) => {
-    await sendMessage({ text: data.message });
-    form.reset();
+  const HandleButton = async (text: string) => {
+    await navigator.clipboard.writeText(text);
+    setTimeout(() => setCopied("Copy"), 3000);
   };
 
   const [isFocused, setIsFocused] = useState(false);
   const { messages, sendMessage } = useChat();
 
-  // TODO : MAke this a toast Component
-  // if (!data) {
-  //   return <div className="bg-red-200 p-2 border">Unauthroized</div>;
-  // }
+  const HandleSubmit = async (data: z.infer<typeof FormSchema>) => {
+    setLoading(true);
+    await sendMessage({ text: data.message, metadata: tone });
+    setLoading(false);
+
+    form.reset();
+  };
 
   const latestMessage = [...messages]
     .reverse()
     .find((msg) => msg.role == "assistant");
+  const ExtractedText =
+    latestMessage?.parts
+      .filter((part) => part.type == "text")
+      .map((part) => part.text)
+      .join(" ") ?? "";
+
   return (
     <div className="mx-auto stretch">
       <form
@@ -56,35 +67,64 @@ const MainContent = () => {
           onBlur={() => setIsFocused(false)}
         />
         <div className="flex items-center justify-between mt-4 ">
-          {/*<p className="text-black/80 dark:text-white/80">Hola</p>*/}
-
-          <CustomDropDown />
+          {/*How to lift state up ? */}
+          <CustomDropDown tone={tone} setTone={setTone} />
 
           <Button
             type="submit"
             className="bg-white/20 dark:bg-black/30 backdrop-blur-md border border-white/10 dark:border-white/20 text-white dark:text-white"
+            // onClick={async () => {
+            //   setComplete(true);
+            //   setTimeout(() => setComplete(false), 3000);
+            // }}
+            // disabled={loading}
           >
-            <ChevronRight />
+            {/*{complete == true ? (
+              <div className="size-1.5 bg-white animate-pulse"></div>
+            ) : (
+              <ChevronRight />
+            )}*/}
+            {loading ? (
+              // <div className="size-1.5 bg-white animate-pulse" />
+              <Loader2 className="animate-spin" />
+            ) : (
+              <ChevronRight />
+            )}
           </Button>
         </div>
       </form>
 
       {latestMessage && (
-        <div className="whitespace-pre-wrap bg-pink-80 max-w-[900px] mx-auto flex flex-col">
-          <div className="mr-auto p-2 mb-2 w-full rounded-xl">
-            {latestMessage.parts.map((part, i) => {
-              switch (part.type) {
-                case "text":
-                  return (
-                    <ScrollArea
-                      className="h-[200px] w-full rounded-md border p-4 bg--500"
-                      key={`${latestMessage.id}-${i}`}
-                    >
-                      {part.text}
-                    </ScrollArea>
-                  );
-              }
-            })}
+        <div>
+          <div className="py-2">
+            <Button
+              className="flex ml-auto "
+              variant={"custom"}
+              onClick={() => {
+                HandleButton(ExtractedText);
+                setCopied("Copied");
+              }}
+            >
+              {copy == "Copy" ? <Copy size={20} /> : <CheckCheck size={20} />}
+            </Button>
+          </div>
+          <div className="whitespace-pre-wrap  max-w-[900px] mx-auto flex flex-col">
+            <div className="mr-auto p-2 mb-2 w-full rounded-xl ">
+              {latestMessage.parts.map((part, i) => {
+                switch (part.type) {
+                  case "text":
+                    return (
+                      <ScrollArea
+                        className=" w-full rounded-md border p-4 "
+                        key={`${latestMessage.id}-${i}`}
+                      >
+                        {/*// This works*/}
+                        <div className="max-h-[13rem]">{part.text}</div>
+                      </ScrollArea>
+                    );
+                }
+              })}
+            </div>
           </div>
         </div>
       )}
