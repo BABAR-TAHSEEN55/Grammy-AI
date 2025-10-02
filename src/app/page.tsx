2e2006ea51839818405a8b788f474edddddba003
Babar <babarsmoke24@gmail.com>
2025-10-02 18:09:54 +0530

    Refactor : Loader

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,29 @@
+"use client";
 import Header from "@/components/Header";
+import Loader from "@/components/Loader";
+
 import MainContent from "@/components/MainContent";
+import { useEffect, useState } from "react";
 
 const Home = () => {
+  const [timer, setTimer] = useState<boolean>(false);
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setTimer(true);
+    }, 3000);
+    return () => clearInterval(timeout);
+  });
   return (
     <main className="grid place-content-center gap-y-2 md:gap-y-8 min-h-[80vh]  relative">
       <Header />
-      <MainContent />
+      {timer ? <MainContent /> : <Loader />}
     </main>
   );
 };
 
 export default Home;
+// TODO :
+// 2. Add Context with backend support
+// 3. Interactions
+// 4. Credits System 2 Credit
+// 4.DEPLOY ( Deadline : 3)
