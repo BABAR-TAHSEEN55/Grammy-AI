7dfa8fc64be4f2f063d90ef870ffcdd2032d31e6
Babar <babarsmoke24@gmail.com>
2025-10-09 18:29:11 +0530

    style : Made some UI Tweaks

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,16 +2,16 @@ import Link from "next/link";
 
 const Footer = () => {
   return (
-    <div className="flex flex-col items-center justify-center mt-8  ">
-      <p className="text-lg">Made with ❤️ by Kim</p>
-      <p className="text-center">
+    <div className="flex flex-col items-center justify-center mt-4  ">
+      <p className="text-lg mb-1 tracking-widest">Made with ❤️ by Kim</p>
+      <p className="text-center tracking-widest">
         Copyright © Suho Kim 2025 All rights Reserved
       </p>
       <Link
         href="https://x.com/Suhokim55"
         target="_blank"
         rel="noopener noreferrer"
-        className=" text-gray-500 hover:text-white transition duration-900 ease-in-out"
+        className=" text-gray-500 hover:text-white transition duration-900 ease-in-out tracking-wider"
       >
         Twitter
       </Link>
